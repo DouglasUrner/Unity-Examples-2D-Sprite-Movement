782e709463d2ada18f6a860552255a5965d4f935
Douglas Urner <dlu@canishe.com>
2019-05-06 22:59:38 -0700

    Starting on collisions.

---
+++ Assets/Scripts/SpriteController2D.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteController2DMinimal : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public string axisX = "Horizontal";
+    public string axisY = "Vertical";
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var moveX = Input.GetAxis(axisX);
+        var moveY = Input.GetAxis(axisY);
+
+        rb.velocity = new Vector2(moveX * speed, moveY * speed);
+    }
+
+    /*
+     * OnCollisionEnter() is called whenever we collide with another game
+     * object with a collider component. We are passed a Collision 2D object,
+     * other, that we can examine to determine what we collided with. When we
+     * collide with an
+     * object the behavior of our sprite and of the object we collided with is
+     * determined by the Unity physics engine - if you just want to detect the
+     * collision, use OnTriggerEnter() and set the IsTrigger property on the
+     * game object's collider.
+     */
+    void OnCollisionEnter2D(Collision2D other) {
+      Debug.Log("OnCollisionEnter2D(): hit " + other);
+        throw new System.NotImplementedException();
+    }
+}
--- Assets/Scripts/SpriteController2DMinimal.cs
+++ Assets/Scripts/SpriteController2DMinimal.cs
@@ -2,24 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpriteController2DMinimal : MonoBehaviour
+public class SpriteController2D : MonoBehaviour
 {
+    /*
+     * Public instance variables are visible in the Unity Inspector, they
+     * are a convenient way to set tuning variables. You can also declare
+     * variables that you would like to watch at runtime as public - even
+     * if you shouldn't be changing them manually.
+     */
     public float speed = 1.0f;
     public string axisX = "Horizontal";
     public string axisY = "Vertical";
 
     private Rigidbody2D rb;
-    
+
     // Start is called before the first frame update
     void Start() {
+        /*
+         * GetComponent() is a relatively expensive method, so by doing it
+         * here and caching the value we reduce the overhead in Update().
+         * Since the update overhead is split across the Update() methods of
+         * all of the game objects, it can add up in non-obvious ways.
+         */
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update() {
+        /*
+         * Get the amount of X and Y axis movement the user is asking for. The
+         * values will be between -1.0 and 1.0, with a middle value of 0 representing
+         * no movement. Negative values represent left and down, positive up and
+         * right.
+         */
         var moveX = Input.GetAxis(axisX);
         var moveY = Input.GetAxis(axisY);
-        
+
         rb.velocity = new Vector2(moveX * speed, moveY * speed);
     }
 }
